20e13a391a8dd1afe3929344b8b5450cd2e334c7
Guru <groovyking2002@gmail.com>
2026-04-01 12:02:30 -0700

    Removed properties dictionary from class definition

--- Group.py
+++ Group.py
@@ -5,24 +5,24 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 
 class Group:
-    def __init__(self, galaxy, properties):
+    def __init__(self, galaxy, m500):
         """
-            Must inlcude M500 in the properties dictionary, and the galaxy must have a redshift (pos[2]) for the calculations to work.
+            Must inlcude M500 in the initialization, and the galaxy must have a redshift (pos[2]) for the calculations to work.
         """
         self.galaxy = galaxy
         self.pos = galaxy.pos
-        self.properties = properties
+        self.m500 = m500
         return
 
     def NFW_cylinder(self, galaxies):
         try:
-            H = self.properties["H"]
+            H = self.H
         except KeyError:
             self.compute_properties()
-            H = self.properties["H"]
-        r200 = self.properties["r200"]
-        sigma_z = self.properties["sigma_z"]
-        Da = self.properties["Da"]
+            H = self.H
+        r200 = self.r200
+        sigma_z = self.sigma_z
+        Da = self.Da
         theta_max = (r200 / Da).to(u.dimensionless_unscaled).value
         z_max = 2 * sigma_z.value
 
@@ -62,14 +62,14 @@ class Group:
     def _NFW_Sigma(self, r):
         """Computes the projected surface density Sigma(r) at a given projected radius r using the NFW profile. Refer to equation 14 in the draft paper."""
         try:
-            r200 = self.properties["r200"]
-            c200 = self.properties["c200"]
-            rho_crit = self.properties["rho_crit"]
+            r200 = self.r200
+            c200 = self.c200
+            rho_crit = self.rho_crit
         except KeyError:
             self.compute_properties()
-            r200 = self.properties["r200"]
-            c200 = self.properties["c200"]
-            rho_crit = self.properties["rho_crit"]
+            r200 = self.r200
+            c200 = self.c200
+            rho_crit = self.rho_crit
 
         # Compute the scale radius rs
         rs = r200 / c200
@@ -87,35 +87,35 @@ class Group:
         """
             Computes R200, M200, c200, and rho_crit for the group based on the input M500 and redshift.
         """
-        m500 = self.properties["M500"]
+        m500 = self.m500
         z = self.pos[2]
 
         # Compute the critical density of the universe at the galaxy's redshift
         H = Hubble_parameter(z)
-        self.properties["H"] = H
+        self.H = H
 
         Da = angular_diameter_distance(z)
-        self.properties["Da"] = Da.to(u.Mpc)
+        self.Da = Da.to(u.Mpc)
 
         rho_crit = 3 * H**2 / (8 * np.pi * G) 
-        self.properties["rho_crit"] = rho_crit.to(u.Msun / u.Mpc**3)
+        self.rho_crit = rho_crit.to(u.Msun / u.Mpc**3)
 
         # Compute the radius R500 corresponding to M500
         r500 = (3 * m500 / (4 * np.pi * 500 * rho_crit))**(1/3)
         r500 = r500.to(u.Mpc)
-        self.properties["r500"] = r500
+        self.r500 = r500
 
         # Compute M200 using the relation r200 = r500 / 0.67
         r200 = r500 / 0.67
-        self.properties["r200"] = r200.to(u.Mpc)
+        self.r200 = r200.to(u.Mpc)
 
         m200 = (4/3) * np.pi * 200 * rho_crit * r200**3
         m200 = m200.to(u.Msun)
-        self.properties["m200"] = m200
+        self.m200 = m200
 
         # Compute the concentration parameter c200 using the Biviano et al. (2017) relation, check equation 17 of draft paper
         c200 = 10**(1.9 - 0.1 * np.log10(m200.to(u.Msun).value))
-        self.properties["c200"] = c200
+        self.c200 = c200
 
         sigma_e = 0.26
         sigma_e2 = sigma_e * np.log(10) / 3
@@ -123,8 +123,8 @@ class Group:
         A = (474.4 * u.km / u.s)**3 * (m500 * E / (1e14 * u.Msun))**(0.94)
         A = A.to(u.km**3 / u.s**3)
         sigma_v = A**(1/3) * np.exp(sigma_e2**2 / 2)
-        self.properties["sigma_v"] = sigma_v.to(u.km / u.s)
+        self.sigma_v = sigma_v.to(u.km / u.s)
 
         sigma_z =  (1 + z) * sigma_v / c
-        self.properties["sigma_z"] = sigma_z.to(u.dimensionless_unscaled)
+        self.sigma_z = sigma_z.to(u.dimensionless_unscaled)
         return
