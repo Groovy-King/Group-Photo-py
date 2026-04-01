6f3f34685b8b1eacdae0e18a7024d5a4707ed072
Guru <groovyking2002@gmail.com>
2026-04-01 10:14:53 -0700

    Updated to include astropy units

--- Galaxy.py
+++ Galaxy.py
@@ -1,6 +1,8 @@
 import numpy as np
+import astropy.units as u
 
 class Galaxy:
     def __init__(self, pos, properties):
         self.pos = pos
         self.properties = properties
+        return
