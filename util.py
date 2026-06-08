Fixed error in handling units

util.py
@@ -40,7 +40,7 @@ def cumulative_luminosity_function(M, M_star = (-21.35 + 5*np.log10(h)) * u.mag,
     """
     x = 10**(0.4 * (M_star.value - M.value))  # Convert absolute magnitude to luminosity relative to L_star
     cumulative_density = Phi_star * gammaincc(alpha + 1, x)  # Analytical expression for the cumulative luminosity function using the regularized incomplete gamma function
-    cumulative_density = cumulative_density.to(u.Mpc**-3) * u.mag  # Convert to number density units
+    cumulative_density = (cumulative_density * u.mag).to(u.Mpc**-3)  # Convert to number density units
     if return_units:
         return cumulative_density  # Return the result with appropriate units
     else: