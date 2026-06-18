b806cee7c637fdc2635da4ece96e792d0ca38117
Guru <groovyking2002@gmail.com>
2026-06-17 20:19:28 -0700

    added print message

--- all_runs_main.py
+++ all_runs_main.py
@@ -2,6 +2,7 @@ import subprocess
 
 for volume in range(1, 10):
     for slice in range(1, 4):
+        print(f"Running main.py with volume={volume} and slice={slice}")
         subprocess.run([
             "python", "main.py",
             "--volume", str(volume),
